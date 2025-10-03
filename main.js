a73a109823fcb0621c575ab5f7b13e13ae6221bf
Javier Pulido <javierpulidorin@gmail.com>
2025-10-02 20:27:34 -0500

    Llamar las funciones al main

--- main.js
+++ main.js
@@ -2,3 +2,16 @@ import { usuarios, agregarUsuario, listarusuario } from "./usuarios.js"
 import { libros, registrar_libro, listar_libros, listar_libros_disponibles, listar_libros_por_autor } from "./libros.js"
 import { prestar_libro, devolver_libro } from "./prestamos.js"
 
+agregarUsuario("Javier","javier.pulido@upb.edu.co")
+listarusuario()
+registrar_libro("El Principito","Antoine de Saint-Exupéry")
+registrar_libro("Harry Potter","J. K. Rowling")
+listar_libros()
+listar_libros_disponibles()
+listar_libros_por_autor("Antoine de Saint-Exupéry")
+prestar_libro("El Principito")
+listar_libros_disponibles()
+devolver_libro("El Principito")
+listar_libros_disponibles()
+
+
